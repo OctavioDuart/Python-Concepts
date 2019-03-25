a0338155139f40ce850f7eb90a8d299a59db4daa
OctavioDuart <octavioestudos@gmail.com>
2019-03-24 22:52:26 -0300

    Dictionary

---
+++ 04dictionary/dictionary.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+
+
+# Declaração
+dictionary  = {
+    "name" : "Octávio",
+    "last_name": "Duarte",
+    "profession": "Developer",
+    "age": 19
+}
+
+# Acessando os Valores
+
+print("My name : %s"  %(dictionary["name"]))      #Octávio
+print("My last : %s"  %(dictionary["last_name"])) #Duarte
+print("My profession %s" %(dictionary["profession"])) #Developer
+print("My age  : %s"  %(dictionary["age"])) #19
+
+
+
+'''
+    Basicamente a sintaxe para acessarmos os valores de um objeto é
+    objeto["chave"]
+'''
